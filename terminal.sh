4b4243a019c11ba64cf8d29bd56f9649b101f547
Roger Searjeant <roger@searjeant.org>
2025-01-02 20:00:13 +0000

    removed mise, replaced with asdf plus some other changes

--- terminal.sh
+++ terminal.sh
@@ -1,6 +1,6 @@
 # Needed for all installers
-# sudo apt update -y
-# sudo apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
 # Run terminal installers
--- terminal/sdks.sh
+++ terminal/sdks.sh
@@ -1,32 +1,12 @@
 # Language SDKs and related.
-# depends on mise being installed - see devbox/terminal/mise.sh
+#
+# I have completely abandoned mise for the time being. Mise is a clever 
+# and well-designed utility but requires more effort to understand and use
+# than I want to invest at the moment.
 
-mise use --global java@latest
-mise use --global go@latest
-mise use --global node@lts
-
-# Make the Erlang / Elixir installs separate scripts which we run in a
-# new shell which will have javac on its PATH, so the Erlang jinterface stuff
-# should be installed.
-bash -c terminal/install-erlang
-bash -c terminal/install-elixir
-
-# # Note that the elixir install depends on Erlang being installed and activated
-# mise use --global erlang 
-# # . /home/roger/.local/share/mise/installs/erlang/27.2/activate
-# mise use --global elixir
-# # The next line installs Hex locally
-# # See: https://hexdocs.pm/mix/1.18.1/Mix.Tasks.Local.Hex.html
-# mise x elixir -- mix local.hex --force
-
-
-# Install after Java SDK.  Because the terminal.sh enumerates the .sh files 
-# in alphabetical order I must either create a .sh file which sorts after sdks.sh
-# or do something like this:
-#bash -c ./install-scala
-# UPDATE: I have decided to take this out of devbox and run it manually.
-# Two reasons:
-# 1. Coursier can also mange JVM versions. This conflicts with mise.
-# 2. cs setup will ask the user to make choices. I'd like devbox to be a hands-off
-#    installer for all of the basics, requiring no (or very little) intervention.
-# 
+source terminal/install-go
+source terminal/install-jdk
+source terminal/install-asdf
+bash terminal/install-erlang
+bash terminal/install-elixir
+# source terminal/install-scala
