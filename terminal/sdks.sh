ccac2aa9a5d90cdbe1cc27c759a731a179dcc1a3
Roger Searjeant <roger@searjeant.org>
2025-01-25 17:58:24 +0000

    Changes to neovim and clojure scripts

--- terminal/sdks.sh
+++ terminal/sdks.sh
@@ -4,12 +4,18 @@
 # and well-designed utility but requires more effort to understand and use
 # than I want to invest at the moment.
 
+sdks="go jdk asdf erlang elixir"
+
 source terminal/install-go
 source terminal/install-jdk
-source terminal/install-asdf
-# Must run these in new shell to pick up asdf. These scripts must not have 
-# any sudo commands in them - the sudo password won't propagate.
-bash terminal/install-erlang
-bash terminal/install-elixir
+source terminal/install-clojure
 
+# source terminal/install-asdf
+#
+# Must run following in new shell to pick up asdf. These scripts must not have 
+# any sudo commands in them - the sudo password won't propagate.
+# bash -c terminal/install-erlang
+# bash -c terminal/install-elixir
+
+# Don't run this from the script tree.
 # source terminal/install-scala
